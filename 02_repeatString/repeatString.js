0620a28765c3ef925cd21d31b80217b0f3fc3a94
Archen <132870045+Archen1898@users.noreply.github.com>
2023-12-28 01:13:41 -0500

    Completed Exercises 1 & 2 and passed all test cases

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(phrase, repetitons) {
+    let newPhrase = "";
+    
+    if(repetitons<0)
+        return 'ERROR';
+    
+    for(let i =0; i<repetitons; i++){
+        newPhrase = newPhrase + phrase;
+    }
+    
+    return newPhrase;
 };
 
 // Do not edit below this line
